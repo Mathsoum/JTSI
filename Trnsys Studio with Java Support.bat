fd1f56758e0043a0b169e138d1ae24798a1697bc
Mathieu SOUM <mathieu.soum@gmail.com>
2013-04-29 16:14:22 +0200

    Suppression du dossier bin du repo. Update du .bat

--- Trnsys Studio with Java Support.bat
+++ Trnsys Studio with Java Support.bat
@@ -1,7 +1,7 @@
 set PATH=%PATH%;C:\Program Files\Java\jdk1.7.0_17\bin;C:\Program Files\Java\jre7\bin;C:\Program Files\Java\jre7\bin\client
 set jtsiserverhost=192.168.1.10
 set jtsiserverport=4444
-set JTSI_JAR=E:\ter\sourceRepos\JTSI\jtsijavapart.jar
+set JTSI_JAR=E:\ter\sourceRepos\JTSI\jtsijavapart.jar;E:\ter\ivy-java.jar
 set JTSI_LOG=E:\ter\out.log
 ::START E:\ter\Trnsys16\Studio\Exe\TrnsysStudio.exe
 START C:\Trnsys17Demo\Studio\Exe\Studio.exe
--- generateJar.sh
+++ generateJar.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-mv jtsijavapart.jar jtsijavapart.jar.back
+mv -v jtsijavapart.jar jtsijavapart.jar.back
 jar cvmf META-INF/MANIFEST.MF jtsijavapart.jar -C ~/Documents/ter/sourceRepos/JTSI/bin .
